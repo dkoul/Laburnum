77bea0bcf4c667827da436694329bc9999e4d93c
Deepak Koul <dkoul@redhat.com>
2019-05-16 15:02:24 +0530

    removed internal references

--- src/test/java/tests/Monitor.java
+++ src/test/java/tests/Monitor.java
@@ -1,8 +1,15 @@
 package tests;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
 import pages.HomePage;
 import utilities.Env;
 
@@ -16,20 +23,41 @@ public class Monitor extends BaseTest {
 
 	{
 
-		this.createDriver("chrome");
-		WebDriver driver = this.getWebDriver();
-		System.out.println("Chrome driver instance created successfully");
-		homepage = new HomePage(driver);
-		homepage.visit();
-		System.out.println("Login page loaded, initiating signin process");
-		if(Env.getUsername()!="")
-		{
-			homepage.signIn();
-						
+		String choice = "UI";
+		if (Env.getHealthLocator() == "") {
+
+			choice = "API";
 		}
-		homepage.checkHealthLocatorPresent();
-		System.out.println("Matched health locator successfully");
+		switch (choice) {
+		case "UI":
+			this.createDriver("chrome");
+			WebDriver driver = this.getWebDriver();
+			System.out.println("Chrome driver instance created successfully");
+			homepage = new HomePage(driver);
+			homepage.visit();
+			if (Env.getUsername() != "") {
+				System.out.println("Login page loaded, initiating signin process");
+				homepage.signIn();
+
+			}
+			homepage.checkHealthLocatorPresent();
+			System.out.println("Matched health locator successfully");
+			break;
+
+		case "API":
+			URL url = new URL(Env.getAppURL());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			if(Env.getUsername()!= "") {
+				String encoded = Base64.getEncoder().encodeToString((Env.getUsername()+":"+Env.getPassword()).getBytes(StandardCharsets.UTF_8));  
+				conn.setRequestProperty("Authorization", "Basic "+encoded);
+			}
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			Assert.assertEquals(conn.getResponseCode(), 200);
+
+		}
 
 	}
 
+	
 }
